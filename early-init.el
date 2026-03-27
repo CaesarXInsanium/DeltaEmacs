adb2ad19c275a0362df1bf8107d717e4a1e3e056
Jesus Ramos Brito <caesarxinsanium.dev@gmail.com>
2026-03-26 19:21:28 -0500

    Some very powerful update. added straight.el

--- early-init.el
+++ early-init.el
@@ -1,3 +1,5 @@
+;; This file is very mysterious for me
 (setq gc-cons-threshold 100000000)
 (setq read-process-output-max (* 1024 1024))
+(setq package-enable-at-startup nil)
 (byte-recompile-directory (expand-file-name user-emacs-directory))
